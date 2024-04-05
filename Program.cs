4d266a9f367d609a0f5112eab2cbd833d544615d
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2024-04-04 21:30:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+    }
+
+    public class Suite
+    {
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public double ValorDiaria { get; set; }
+
+        public Suite(string tipoSuite, int capacidade, double valorDiaria)
+        {
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiaria = valorDiaria;
+        }
+    }
+
+    public class Reserva
+    {
+        private Suite _suite;
+        private List<Pessoa> _hospedes;
+        public int DiasReservados { get; set; }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+            _hospedes = new List<Pessoa>();
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            _suite = suite;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            _hospedes.AddRange(hospedes);
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return _hospedes.Count;
+        }
+
+        public double CalcularValorDiaria()
+        {
+            double valorTotal = _suite.ValorDiaria * DiasReservados;
+            if (DiasReservados > 10)
+            {
+                valorTotal *= 0.9; 
+            }
+            return valorTotal;
+        }
+    }
+}
